e13eb5860f326e490d29b6c274e6a8746081d2fb
Pedro Anuda <pedroanuda@gmail.com>
2025-06-22 17:28:55 -0300

    first commit

---
+++ src/main/java/com/empacoters/ants_at_work_backend/AntsAtWorkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.empacoters.ants_at_work_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntsAtWorkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntsAtWorkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empacoters/ants_at_work_backend/AntsAtWorkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empacoters.ants_at_work_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntsAtWorkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
