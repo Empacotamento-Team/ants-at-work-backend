e3ad587d7ac0c96810ce7825cdda659fb2b11715
Pedro Anuda <pedroanuda@gmail.com>
2025-08-27 00:39:09 -0300

    fix(SecurityConfig): permit OPTIONS method to all requests

--- src/main/java/com/empacoters/antsback/identity/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/empacoters/antsback/identity/infrastructure/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.empacoters.antsback.identity.infrastructure.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +22,7 @@ public class SecurityConfig {
         return http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/auth/register").authenticated()
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/error").permitAll()
