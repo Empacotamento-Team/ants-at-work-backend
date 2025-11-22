bc2985b0b0ba56610aec8d6c65a5bdf55a0bdd6e
Pedro Anuda <pedroanuda@gmail.com>
2025-11-22 09:53:30 -0300

    ✨ feat: add support to async

---
+++ src/main/java/com/empacoters/antsback/config/AsyncConfig.java
@@ -0,0 +1,20 @@
+package com.empacoters.antsback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig {
+    @Bean(name = "asyncExecutor")
+    public TaskExecutor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(30);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Async-Executor-");
+        executor.initialize();
+        return executor;
+    }
+}
