1f89ca8501c4c52e338bc7ed9958a3d9f8aae581
Pedro Anuda <pedroanuda@gmail.com>
2025-12-01 19:57:19 -0300

    🐛 fix: adjust migration v11

--- src/main/resources/db/migration/V11__add_model_id_to_foreign_key.sql
+++ src/main/resources/db/migration/V11__add_model_id_to_foreign_key.sql
@@ -1,5 +1,11 @@
 ALTER TABLE trucks
-ADD CONSTRAINT fk_trucks_model
-FOREIGN KEY (model_id) REFERENCES truck_models(id)
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+    ADD model_id BIGINT NULL;
+
+ALTER TABLE trucks
+    ADD CONSTRAINT FK_TRUCKS_ON_MODEL FOREIGN KEY (model_id) REFERENCES truck_models (id);
+
+UPDATE trucks
+SET maximum_capacity = '0'
+WHERE maximum_capacity IS NULL;
+ALTER TABLE trucks
+    MODIFY maximum_capacity DOUBLE NOT NULL;
